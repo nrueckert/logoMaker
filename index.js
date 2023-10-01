469cf49430acbe933160dede23d55f614dd0f501
Nicolai Rueckert <nrueckert@Nicolais-MacBook-Pro.local>
2023-10-01 01:44:14 -0500

    90% done, JESTS and a small kink left

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
 const inquirer = require('inquirer');
-const Triangle = require('./lib/shapes')
 const fs = require('fs')
+
+const Circle = require('./lib/shapes.js')
+const Rectangle = require('./lib/shapes.js')
+const Triangle = require('./lib/shapes.js')
 // && input.val == /^[a-zA-Z]$/
 
 const questions = [
     {
-        type: "input",
-        name: "text",
-        message: "What letters do you want to use? (Max 3)",
+        type: 'input',
+        name: 'text',
+        message: 'What letters do you want to use? (Max 3)',
         validate: function(input){
             if(input.length < 4){
                 return true
             } else {
-                return "3 Letters Only"
+                return '3 Letters Only'
             }
             
         }
     },
     {
-        type: "input",
-        name:"textColor",
-        message:"What color do you want the text to be?"
+        type: 'input',
+        name:'textColor',
+        message:'What color do you want the text to be?'
     },
     {
-        type: "list",
-        name: "shape",
-        message: "What shape do you want to use?",
+        type: 'list',
+        name: 'shape',
+        message: 'What shape do you want to use?',
         choices: [
-            "Triangle",
-            "Circle",
-            "Rectangle",
+            'Triangle',
+            'Circle',
+            'Rectangle',
         ]
     },
     {
-        type: "input",
-        name: "shapeColor",
-        message: "What color do you want the shape to be?",
+        type: 'input',
+        name: 'shapeColor',
+        message: 'What color do you want the shape to be?',
 
     }
 ]  
@@ -47,9 +50,38 @@ inquirer.prompt(questions).then(answers => {
 });
 
 function createSVG(answers) {
-    if(answers.shape == 'Triangle'){
-       Triangle.shapeColor = answers.shapeColor
-       Triangle.text = answers.text
-       Triangle.textColor = answers.textColor
+    console.log(answers)
+    if(answers.shape = 'Circle'){
+       const newCircle = new Circle(answers.text, answers.textColor, answers.shapeColor).render()
+       fs.writeFile('./logo.svg', newCircle, (err) => {
+            if(err){
+                console.log('Could not write file')
+            } else {
+                console.log('Write Successful')
+            }
+       }
+       )
+    } else if (answers.shape = 'Triangle'){
+        const newTriangle = Triangle(answers.text, answers.textColor, answers.shapeCholor).render()
+        fs.writeFile('./logo.svg', newTriangle, (err) => {
+            if(err){
+                console.log('Could not write file')
+            } else {
+                console.log('Write Successful')
+            }
+        })
+    } else if (answers.shape = 'Rectangle'){
+        const newRectangle = new Rectangle(answers.text, answers.textColor, answers.shapeColor).render()
+        fs.writeFile('./logo.svg', newRectangle, (err) => {
+            if(err){
+                console.log('Could not write file')
+            } else {
+                console.log('Write Successful')
+            }
+        })
+
+    } else {
+        console.log('Unknown issue occured in shape selection')
     }
 }
+
